b63a07e6e202db652c2d7ac80eeb8e0736b22e3d
root <fionamuthoni18@gmail.com>
2023-08-01 10:26:32 +0000

    babel

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -11,6 +11,7 @@ app = Flask(__name__)
 
 
 class Config:
+    """Represents a Flask Babel configuration."""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
